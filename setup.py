fb844da59c4323e495c61c5c138779f880531af0
nathanaday <79942554+nathanaday@users.noreply.github.com>
2021-04-06 18:13:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup
 
 setup(
     name='SatellitePrediction',
-    version='',
-    packages=[''],
-    url='',
-    license='',
+    version='1',
+    url='https://github.com/nathanaday/SatellitePrediction',
     author='nathanaday',
     author_email='nraday1221@gmail.com',
-    description=''
+    description='A tool for predicting satellite observation opportunities over a location'
 )

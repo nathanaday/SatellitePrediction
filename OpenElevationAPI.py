860b14d373b254cdb436422dd4b83157083ad3e3
nathanaday <nraday1221@gmail.com>
2024-04-18 12:12:28 -0700

    Updated ignored directories

--- OpenElevationAPI.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-lat = 41.161758
-lon = -8.583933
-url = r"https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933"
-
-res = requests.get(url)
-print(res.json())
